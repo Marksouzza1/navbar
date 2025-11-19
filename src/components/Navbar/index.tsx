3a287190c13c2a90baa51443cbb6d5fcc0924a56
Marksouzza1 <marcogs713@gmail.com>
2025-11-19 16:11:08 -0300

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,12 +6,25 @@ export default function Navbar() {
     <header>
       <nav className="navbar">
         <Link href="/">
-          <Image 
-            src='logo.png' 
-            width={20} 
-            height={20} 
-            alt="logo"/>
+          <Image src="logo.png" width={20} height={20} alt="logo" />
         </Link>
+
+        <ul className="nav-items">
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              Inicio
+            </Link>
+            <Link href="/about" className="nav-link">
+              Sobre
+            </Link>
+            <Link href="/products" className="nav-link">
+              Produtos
+            </Link>
+            <Link href="/Services" className="nav-link">
+              Serviçps
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
